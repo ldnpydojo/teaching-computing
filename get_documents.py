6aa9d868c2787d083a246d6e81772f3e71ce3bdd
richardasaurus <richard@richard.do>
2014-03-07 15:40:21 +0000

    Seperated out into functions. Reduced unneeded looping

--- get_documents.py
+++ get_documents.py
@@ -2,13 +2,30 @@ from imdbpie import Imdb
 import json
 
 
-def get_documents(number_of_films=10):
-    imdb = Imdb({'anonymize': False})
-    titles_and_ids = [{'title': x['title'], 'id': x['tconst']}  for x in imdb.top_250()[:number_of_films]]
-    titles_and_plots = [{'title': x['title'], 'plot': imdb.find_movie_by_id(x['id']).plot['outline'] } for x in titles_and_ids]
-    titles_and_tokens = {x['title']: list(set(x['plot'].split())) for x in titles_and_plots}
+imdb = Imdb({'anonymize': False})
+
+
+def _tokenize_plot(plot):
+    plot_words_unique = set(plot.split())
+    # return as a list since python cant serialize
+    # a set to JSON
+    return list(plot_words_unique)
+
+
+def _get_title_plot(top_movie):
+    # Get the movie data from Imdb
+    movie = imdb.find_movie_by_id(top_movie['tconst'])
+    # Return the title and tokenized plot keyword list
+    plot = movie.plot['outline']
+    return movie.title, _tokenize_plot(plot)
+
+
+def get_documents(number_of_movies=10):
+    top_movies = imdb.top_250()[:number_of_movies]
+    titles_and_tokens = dict([_get_title_plot(x) for x in top_movies])
+
+    # return the final documents in json format for easy ingestion
     return json.dumps(titles_and_tokens)
 
 if __name__ == '__main__':
-    import sys
-    print get_documents(2)
+    print get_documents()
